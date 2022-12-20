db1c9e357e6f99a6b1b14cdd2de6fef35a1cfefa
Jacob Venable <jacob.venable@gmail.com>
2022-12-20 09:40:37 -0500

    Revert "add custom metro config"
    This reverts commit d3fa3dffee2319cee639a13dd70f7d67f2f55d31.

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,46 +1,18 @@
-/* eslint-disable @typescript-eslint/no-var-requires, import/no-extraneous-dependencies */
-const resolver = require("metro-resolver");
-const { mergeConfig } = require("metro-config");
-const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
-const expoMetroConfig = getDefaultConfig(__dirname);
-const blacklistedModules = ["@react-aria/interactions"];
-// const exclusionList = require("metro-config/src/defaults/exclusionList");
-
+// Find the workspace root, this can be replaced with `find-yarn-workspace-root`
+const workspaceRoot = path.resolve(__dirname, '../..');
 const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, "../..");
 
-const newConfig = {
-  watchFolders: [workspaceRoot],
-  resolver: {
-    disableHierarchicalLookup: true,
-    nodeModulesPaths: [
-      path.resolve(projectRoot, "node_modules"),
-      path.resolve(workspaceRoot, "node_modules"),
-    ],
-    resolveRequest: (context, realModuleName, platform) => {
-      if (blacklistedModules.includes(realModuleName)) {
-        return {
-          filePath: path.resolve(path.join(__dirname, "/src/shim-module.js")),
-          type: "sourceFile",
-        };
-      }
-      const routesRegex = /\.\/routes/;
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { resolveRequest, ...restContext } = context;
-      const module =
-        routesRegex.test(realModuleName) &&
-        process.env.REACT_NATIVE_APPLICATION_NAME === "hub-br"
-          ? realModuleName.replace(
-              routesRegex,
-              `./routes.${process.env.REACT_NATIVE_APPLICATION_NAME.slice(-2)}`
-            )
-          : realModuleName;
-      return resolver.resolve(restContext, module, platform);
-    },
-    sourceExts: ["js", "jsx", "ts", "tsx", "json", "cjs"],
-  },
-};
+const config = getDefaultConfig(projectRoot);
 
-module.exports = mergeConfig(expoMetroConfig, newConfig);
+// 1. Watch all files within the monorepo
+config.watchFolders = [workspaceRoot];
+// 2. Let Metro know where to resolve packages, and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+module.exports = config;

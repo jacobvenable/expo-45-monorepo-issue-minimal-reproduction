685975ea39fd336f9921af836de57f186db26346
Jacob Venable <jacob.venable@gmail.com>
2022-12-20 09:18:20 -0500

    feat: add sanitize-html as dep test

--- packages/expo-custom/src/index.tsx
+++ packages/expo-custom/src/index.tsx
@@ -1,7 +1,23 @@
-import * as React from 'react';
+import * as React from "react";
 // @ts-ignore
-import { Text } from 'react-native';
+import { Text } from "react-native";
+
+import sanitizeHtml, { IOptions } from "sanitize-html";
+
+const useSanitizeHtml = (
+  content: string,
+  options: IOptions = {
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(["style"]),
+    allowedAttributes: (sanitizeHtml.defaults.allowedAttributes = {
+      "*": ["style", "class", "lang", "href", "data-native-link"],
+    }),
+  }
+) => {
+  return sanitizeHtml(content, options);
+};
 
 export function MyView() {
+  const why = useSanitizeHtml("why are we using this?");
+
   return <Text>Hello from the first package</Text>;
 }
